0fff32bffa1ab06d861472cbdd24f796f68022dc
Satadru Roy <satadru.roy98@gmail.com>
2021-01-08 04:08:52 +0530

    <ImageContainer /> reacts to change in sizes

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -1,34 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
-// import useResizeAware from 'react-resize-aware';
+import React, { useState, useEffect } from 'react';
+import useResizeAware from 'react-resize-aware';
 
 import classes from './ImageContainer.module.scss';
 
 const ImageContainer = props => {
   const [style, setStyle] = useState(null);
-  const imgRef = useRef();
+  const [resizeListener, sizes] = useResizeAware();
 
   useEffect(() => {
     console.log('triggered');
     switch (props.setheightorwidth) {
       case 'width':
         setStyle({
-          height: `${imgRef.current.clientWidth}px`,
+          height: `${sizes.width}px`,
         });
         break;
 
       case 'height':
         setStyle({
-          width: `${imgRef.current.clientHeight}px`,
+          width: `${sizes.height}px`,
         });
         break;
 
       default:
         setStyle(null);
     }
-  }, [props.setheightorwidth]);
+  }, [props.setheightorwidth, sizes.width, sizes.height]);
 
   return (
-    <div className={classes.ImageContainer} ref={imgRef} style={style}>
+    <div className={classes.ImageContainer} style={{ position: 'relative', ...style }}>
+      {resizeListener}
       <img {...props} alt={props.alt} />
     </div>
   );
